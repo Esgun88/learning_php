e17af96f22946c31dbcd7e52de846afe5d862baf
esgun <radiovideostar@gmail.com>
2019-12-18 19:58:02 +0900

    POST와 GET방식을 활용하여 로그인 해보기

---
+++ index.php
@@ -0,0 +1,33 @@
+
+<html>
+<head>
+
+<?php
+$a = 1;
+echo $a+1;
+echo "<br/>";
+$a = 100;
+print $a+1;
+define('this', 'i am a boy man fuk');
+echo this;
+echo "<br/>";
+echo "<br/>";
+echo "<br/>";
+
+if ($_POST[id] == esgun) {
+    if ($_POST[password] == dlfemd) {
+        echo "\"esgun\"에 대한 정보가 일치합니다";
+    }
+    else {
+        echo "비번은 틀렸어 임마";
+    }
+    
+}
+else {
+    echo "제대로좀 해라";
+}
+
+?>
+
+</head>
+</html>
